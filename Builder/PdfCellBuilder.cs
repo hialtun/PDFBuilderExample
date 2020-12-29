e7ed2d5c34c3c9e72ee86d1ea5684a5d20599b58
hialtun <halilibrahimaltun1@gmail.com>
2020-12-29 23:40:39 +0300

    init

---
+++ Builder/PdfCellBuilder.cs
@@ -0,0 +1,74 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFBuilerExample.Builder
+{
+    public class PdfCellBuilder
+    {
+        private PdfPCell PdfPCell { get; set; }
+        private Font DefaultFont { get; set; }
+        public PdfCellBuilder(){
+            PdfPCell = new PdfPCell();
+            DefaultFont = new Font(Font.FontFamily.TIMES_ROMAN ,12, Font.NORMAL);
+        }
+        public PdfCellBuilder(PdfPCell pdfPCell) {
+            PdfPCell = pdfPCell;
+        }
+        public PdfCellBuilder FontSize(int size)
+        {
+            DefaultFont.Size = size;
+            return this;
+        }
+        public PdfCellBuilder FontStyle(int style)
+        {
+            DefaultFont.SetStyle(style);
+            return this;
+        }
+        public PdfCellBuilder Phrase(string text)
+        {
+            PdfPCell.Phrase = new Phrase(text, DefaultFont);
+            return this;
+        }
+        public PdfCellBuilder BackgroundColor(BaseColor cellColor)
+        {
+            PdfPCell.BackgroundColor = cellColor;
+            return this;
+        }
+        public PdfCellBuilder Colspan(int col)
+        {
+            PdfPCell.Colspan = col;
+            return this;
+        }
+        public PdfCellBuilder Border(int borderType)
+        {
+            PdfPCell.Border = borderType;
+            return this;
+        }
+        public PdfCellBuilder HorizontalAlignment(int alignment)
+        {
+            PdfPCell.HorizontalAlignment = alignment;
+            return this;
+        }
+        public PdfCellBuilder VerticalAlignment(int alignment)
+        {
+            PdfPCell.VerticalAlignment = alignment;
+            return this;
+        }
+        public PdfCellBuilder Padding(int padding)
+        {
+            PdfPCell.Padding = padding;
+            return this;
+        }
+        public PdfPCell Build()
+        {
+            return PdfPCell;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using PDFBuilerExample.Builder;
+using System;
+using System.IO;
+using static iTextSharp.text.Font;
+
+namespace PDFBuilerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Document doc = new Document(new Rectangle(595, 842), 0, 0, 5, 5);
+            string filePath = @"C:\temp\";
+            filePath += "builder.pdf";
+            var fileStream = new FileStream(filePath, FileMode.Create);
+            doc.SetPageSize(PageSize.A4.Rotate());
+            var writer = PdfWriter.GetInstance(doc, fileStream);
+            doc.Open();
+
+            string dummyText1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. " +
+                "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
+                "when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+            string dummyText2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."; 
+            
+            // with pattern
+            PdfPTable pdfTable1 = new PdfPTable(3);
+            pdfTable1.AddCell(
+                    new PdfCellBuilder().Colspan(2).FontSize(14).FontStyle(BOLD).Border(Rectangle.NO_BORDER)
+                        .HorizontalAlignment(Element.ALIGN_CENTER).Phrase("Kolon 1").BackgroundColor(BaseColor.LIGHT_GRAY).Build()
+                );
+            pdfTable1.AddCell(
+                    new PdfCellBuilder().Colspan(1).FontSize(14).FontStyle(BOLD).Border(Rectangle.NO_BORDER)
+                        .HorizontalAlignment(Element.ALIGN_CENTER).Phrase("Kolon 2").BackgroundColor(BaseColor.LIGHT_GRAY).Build()
+                );
+            pdfTable1.AddCell(
+                    new PdfCellBuilder().Colspan(2).FontSize(11).Phrase(dummyText1).Build()
+                );
+            pdfTable1.AddCell(
+                    new PdfCellBuilder().Colspan(1).FontSize(11).Phrase(dummyText2).Build()
+                );
+            doc.Add(pdfTable1);
+
+            doc.Add(new Phrase(" "));
+
+            // without pattern
+            PdfPTable pdfTable2 = new PdfPTable(3);
+            Font Font14Bold = new Font(FontFamily.TIMES_ROMAN, 14, BOLD);
+            PdfPCell PdfPCell = new PdfPCell()
+            {
+                Colspan = 2, HorizontalAlignment = Element.ALIGN_CENTER, BackgroundColor = BaseColor.GRAY, Border = Rectangle.NO_BORDER,
+                Phrase = new Phrase("Kolon 1", Font14Bold)
+            };
+            pdfTable2.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell()
+            {
+                Colspan = 1, HorizontalAlignment = Element.ALIGN_CENTER, BackgroundColor = BaseColor.GRAY, Border = Rectangle.NO_BORDER,
+                Phrase = new Phrase("Kolon 2", Font14Bold)
+            };
+            pdfTable2.AddCell(PdfPCell);
+            Font Font11Normal = new Font(FontFamily.TIMES_ROMAN, 11, NORMAL);
+            PdfPCell = new PdfPCell()
+            {
+                Colspan = 2, Phrase = new Phrase(dummyText1, Font11Normal)
+            };
+            pdfTable2.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell()
+            {
+                Colspan = 1, Phrase = new Phrase(dummyText2, Font11Normal)
+            };
+            pdfTable2.AddCell(PdfPCell);
+            doc.Add(pdfTable2);
+
+            doc.Close();
+            fileStream.Close();
+        }
+    }
+}
